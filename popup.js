ec351a2ffabda429963fe878587ff400b931bd68
Sebastian Hüftlein <sebastian.hueftlein@gmail.com>
2023-08-24 17:54:11 +0200

    update app vesion

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ const init = {
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-site",
-    "x-app-version": "7.3.2",
+    "x-app-version": "7.3.3",
     // "x-titan-token": ""
   },
   referrer: "https://marvelstrikeforce.com/",
